dbfa955d17c09898bd9393aa188166085cb29f28
Yash Patil <yashp9145@gmail.com>
2023-07-14 02:45:41 +0530

    test

--- src/Components/Transactions/Transactions.js
+++ src/Components/Transactions/Transactions.js
@@ -2,7 +2,7 @@ import React,{ useState } from 'react';
 import Button from 'react-bootstrap/Button';
 function Transactions(props) {
     return(
-        <Button variant="primary" href="upi://pay?pa=9145135230@upi&amp;pn=yy &amp;cu=INR">Primary</Button>
+        <Button variant="primary" href="upi://pay?pa=9145135230@upi&amp;pn=Yash%20Patil &amp;cu=INR">Primary</Button>
         
     )
 }
